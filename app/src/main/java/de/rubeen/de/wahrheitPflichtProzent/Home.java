9af968afd02121dcd12b2230ee0ce4d3a06f723f
Ruben Vitt <dev@rubeen.dev>
2020-05-11 13:34:08 +0200

    init(project)

---
+++ app/src/main/java/de/rubeen/de/wahrheitPflichtProzent/Home.java
@@ -0,0 +1,21 @@
+package de.rubeen.de.wahrheitPflichtProzent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
